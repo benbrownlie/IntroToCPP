645e40642b9cfc305fd4643958f58d437f7a60b6
Benjamin Brownlie <benjibspam@gmail.com>
2020-12-08 17:18:57 -0600

    Number Guessing Game
    completed task

--- IntroToCPP/Main.cpp
+++ IntroToCPP/Main.cpp
@@ -1,24 +1,56 @@
 #include <iostream>;
 
+void guessNumber()
+{
+	int min = 1;
+	int max = 10;
+	int answer = 0;
+	int userInput = 0;
+
+	while (min <= max)
+	{
+		answer = (min + max) / 2;
+
+		std::cout << "Is the number " << answer << " ";
+		std::cout << " | 1. Yes | 2. No";
+		std::cin >> userInput;
+
+		std::cout << std::endl;
+
+		if (userInput == 1)
+		{
+			std::cout << "Nice";
+			return;
+		}
+
+		std::cout << " | 1.Higher or 2.Lower? | ";
+		std::cin >> userInput;
+		std::cout << std::endl;
+
+		if (userInput == 1)
+		{
+			min = answer + 1;
+		}
+
+		if (userInput == 2)
+		{
+			max = answer - 1;
+		}
+
+
+	}
+
+}
+
 int main()
 {
+	
+
+	std::cout << "Welcome to the number guessing game!";
+	guessNumber();
+
+
 	int num = 0;
-	std::cout << num << std::endl;
-
-	std::cin >> num;
-
-	if (num == 3 || num%3 == 0)
-	{
-		std::cout << "Fizz: " << num << std::endl;
-	}
-	else if (num == 5 || num % 5 == 0)
-	{
-		std::cout << "Buzz: " << num << std::endl;
-	}
-	else if (num % 5 == 0 && num % 3 == 0)
-	{
-		std::cout << "FizzBuzz: " << num << std::endl;
-	}
 
 	system("pause");
 	return 0;
