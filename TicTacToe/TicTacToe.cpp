7d8521cbb3329125e7dfab8fe95f7239b05040fb
Benjamin Brownlie <benjibspam@gmail.com>
2021-02-05 14:27:59 -0600

    Redid TicTacToe again
    I wont be able to finish TicTacToe game in time, going to turn in as is.

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,88 +1,82 @@
 #include <iostream>
 
-char board[3][3] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+char board[3][3] = { { 0,0,0 }, {0,0,0}, {0,0,0} } ;
 bool gameOver = false;
+char player1[24];
+bool player1Turn = true;
+char player2[24];
 int choice;
-char token1;
-char token2;
-char player1[20];
-char player2[20];
+
+void getName()
+{
+	//Asks for player names and sets player1/player2 char array to entered name
+	std::cout << "Hello player! Please enter a name" << std::endl;
+	std::cin >> player1;
+	std::cout << "Welcome " << player1 << std::endl;
+
+	std::cout << "Hello player2! Please enter a name" << std::endl;
+	std::cin >> player2;
+	std::cout << "Welcome " << player2 << std::endl;
+}
 
 void drawBoard()
 {
-    //Draws out the tic tac toe board
-    for (int i = 0; i < 3; i++)
-    {
-        for (int n = 0; n < 3; n++)
-        {
-            board[i][n] = '0';
-            std::cout << " " << board[i][n];
-        }
-        std::cout << std::endl;
-    }
+	//Draws out the tic tac toe board
+	for (int i = 0; i < 3; i++)
+	{
+		for (int n = 0; n < 3; n++)
+		{
+			board[i][n] = '0';
+			std::cout << " " << board[i][n];
+		}
+		std::cout << std::endl;
+	}
 }
 
-void TicTacToe()
+void ticTacToe()
 {
+	if (player1Turn)
+	{
+		std::cout << "It's your turn " << player1 << " make your move" << std::endl;
+	}
 
-    while (!gameOver)
-    {
-        system("cls");
-    }
+	else
+	{
+		std::cout << "It's your turn " << player2 << " make your move" << std::endl;
+	}
 
+	if (player1Turn)
+	{
+		std::cin >> choice;
+		switch(choice)
+		{
+		case 1:
+			if (choice == 1 && board[0][0] == '0')
+			{
+				board[0][0] = 'X';
+				player1Turn = !player1Turn;
+			}
+			break;
 
-}
+		case 2:
+			if (choice == 2 && board[0][1] == '0')
+			{
+				board[0][1] = 'X';
+				player1Turn = !player1Turn;
+			}
 
-void makeChoice()
-{
-    std::cin >> choice;
-    switch (choice)
-    {
-    case 1:
-
-        break;
-    }
-}
-
-void checkWin()
-{
-    for (int i = 0; i < 3; i++)
-    {
-        if (board[0][i] == 1 &&
-            board[1][i] == 1 &&
-            board[2][i] == 1)
-        {
-            gameOver == true;
-        }
-
-        else if (board[i][0] == 1 &&
-            board[i][1] == 1 &&
-            board[i][2] == 1)
-        {
-            gameOver == true;
-        }
-    }
-
-    if (board[0][0] == 1
-        && board[1][1] == 1
-        && board[2][2] == 1)
-    {
-        gameOver == true;
-    }
-
-    else if (board[0][2] == 1
-        && board[1][1] == 1
-        && board[2][0] == 1)
-    {
-        gameOver == true;
-    }
+		}
+	}
 }
 
 int main()
 {
-    //std::cout << "Welcome to Tic Tac Toe!" << "Player1 please enter a name!";
-    //std::cout << std::endl;
-    drawBoard();
+	getName();
 
-    system("pause");
+	while (!gameOver)
+	{
+		drawBoard();
+		ticTacToe();
+		system("pause");
+	}
 }
